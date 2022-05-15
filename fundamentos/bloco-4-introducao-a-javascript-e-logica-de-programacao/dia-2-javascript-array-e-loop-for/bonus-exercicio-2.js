487253729d87f0ccfa1fc4d903d2217da0aef861
Leckar <gusleckar@gmail.com>
2022-05-15 16:57:14 -0300

    finished day 2 exercises

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus-exercicio-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus-exercicio-2.js
@@ -2,11 +2,27 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//Eu fiz assim:
+// function bubbleSortDescend(array){
+
+// 	for (let index = (array.length -1); index >= 0; index -= 1) {
+// 		for (let secondIndex = array.length; secondIndex > index; secondIndex -= 1) {
+// 			if (array[index] < array[secondIndex]) {
+// 				let position = array[index];
+// 				array[index] = array[secondIndex];
+// 				array[secondIndex] = position;
+// 			}
+// 		}
+// 	}
+// }
+
+
+//E depois vi que podia simplesmente ser assim:
 function bubbleSortDescend(array){
 
-	for (let index = (array.length -1); index >= 0; index -= 1) {
-		for (let secondIndex = array.length; secondIndex > index; secondIndex -= 1) {
-			if (array[index] < array[secondIndex]) {
+	for (let index = 1; index < array.length; index += 1) {
+		for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+			if (array[index] > array[secondIndex]) {
 				let position = array[index];
 				array[index] = array[secondIndex];
 				array[secondIndex] = position;
